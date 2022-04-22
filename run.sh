5bdd95905c1bee664b795e8059b4c005cab70678
pwndrer4 <53749719+pwndrer4@users.noreply.github.com>
2022-04-22 11:08:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-wget -O /dev/null --debug --header="User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1" http://apt.cdalei.com/
+wget -O /dev/null https://apt.cdalei.com/zfbhb.png
 sh run.sh
